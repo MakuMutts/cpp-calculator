1f45db0a2434f3bae1065e57bfb896ac4171bb2d
Viktoriya <kazminaviktoriya@gmail.com>
2024-12-01 11:02:34 +0300

    Калькулятор версия 1.0

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,131 @@
-// В этом файле определения функций.
-// За основу возьмите решение предыдущей задачи.
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "calculator.h"
+
+using Number = double;
+
+bool ReadNumber(Number& result){
+    if(std::cin.fail()){
+        std::cerr << "Error: Numeric operand expected" << std::endl;
+        return 0;
+    }
+    else{
+        return result;
+    }
+    return -1;
+}
+
+bool RunCalculatorCycle(){
+    Number result;
+    std::cin >> result;
+    if(!ReadNumber(result)){
+        return 0;
+    }
+    else {
+        Number Memory = 0;
+        bool is_memory = false;
+        
+        for(int i = 0; i <= 1000; ++i){
+                std::string sign;
+                std::cin >> sign;
+            
+                if(sign =="+"){
+                    Number the_number_read;
+                    std::cin >> the_number_read;
+                    if(std::cin.fail()){
+                        std::cerr << "Error: Numeric operand expected" << std::endl;
+                        break;
+                    }
+                    else{
+                        result += the_number_read;}
+                }
+            
+                else if(sign =="-"){
+                    Number the_number_read;
+                    std::cin >> the_number_read;
+                    if(std::cin.fail()){
+                        std::cerr << "Error: Unknown token " << std::endl;
+                        break;
+                    }
+                    else{
+                        result -= the_number_read;}
+                }
+            
+                else if(sign =="*"){
+                    Number the_number_read;
+                    std::cin >> the_number_read;
+                    if(std::cin.fail()){
+                        std::cerr << "Error: Unknown token " << std::endl;
+                        break;
+                    }
+                    else{
+                        result *= the_number_read;}
+                }
+            
+                else if(sign =="/"){
+                    Number the_number_read;
+                    std::cin >> the_number_read;
+                    if(std::cin.fail()){
+                        std::cerr << "Error: Unknown token " << std::endl;
+                        break;
+                    }
+                    else{
+                        result /= the_number_read;}
+                }
+            
+                else if(sign =="**"){
+                    Number the_number_read;
+                    std::cin >> the_number_read;
+                    if(std::cin.fail()){
+                        std::cerr << "Error: Unknown token " << std::endl;
+                        break;
+                    }
+                    else{
+                        result = pow(result,the_number_read);}
+                }
+            
+                else if(sign == "="){
+                    std::cout << result << std::endl;
+                }
+            
+                else if(sign ==":"){
+                    Number the_number_read;
+                    std::cin >> the_number_read;
+                    if(std::cin.fail()){
+                        std::cerr << "Error: Unknown token " << std::endl;
+                        break;
+                    }
+                    else{
+                        result = the_number_read;}
+                }
+            
+                else if(sign =="c"){
+                    result = 0;
+                }
+            
+                else if(sign =="q"){
+                    break;
+                }
+                else if(sign == "s"){
+                    Memory = result;
+                    is_memory = true;
+                }
+                else if(sign == "l"){
+                    if(is_memory){
+                          result = Memory;
+                    } else {
+                        std::cerr << "Error: Memory is empty" << std::endl;
+                        break;
+                    }
+                }
+                else {
+                    std::cerr << "Error: Unknown token " << sign << std::endl;
+                    break;
+                }
+
+        } 
+    }
+    return 0;
+}            
--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,1 +1,8 @@
-// В этом файле только функция main и #include-директивы.
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "calculator.h"
+
+int main() {
+    RunCalculatorCycle();
+}
